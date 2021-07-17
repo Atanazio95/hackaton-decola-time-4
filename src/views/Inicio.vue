4b0ef3a066472320b1d6434a9d3e836276e2817a
Atanazio95 <atanaziojeorge@gmail.com>
2021-07-17 17:52:21 -0300

    Criação de rotas

---
+++ src/views/Inicio.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <v-container>
+    <img class="logo" src="https://naometoque.prefonline.com.br/wp-content/uploads/sites/20/2018/04/Saude.png" alt="Logo">
+      <h2>
+        Apresentação
+        </h2> 
+        <p>
+          É um site que fomenta conexões entre profissionais da saúde a pessoas que buscam encontrar formas de entretenimento e auto-cuidado dentro do ambiente seguro de casa, visando o ambiente de isolamento social da pandemia, o site visa compartilhar contatos de profissionais que criam conteúdos de auto-cuidado, lazer e diversão que aliviam o estresse psicológico de estar limitado ao um ambiente a maior parte do tempo e trazem formas acessíveis de cuidado físico e psicológico através de atividades integrativas e guiadas, onde conectamos você ao profissional de saúde usando programas de reunião acessíveis e gratuitos, com o profissional guiando a atividade e outras pessoas que vão te acompanhar e compartilham os mesmos interesses que você!
+        </p> 
+  </v-container>
+</template>
+
+<script>
+  export default {
+  name: 'Inicio'
+
+
+    
+  }
+</script>
+
+<style scoped>
+.logo {
+  
+  height: 35vh;
+  width: 90vw;
+}
+</style>
