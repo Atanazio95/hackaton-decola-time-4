03cf75a76d6e9627e67afe121029ad30029de148
Daniel Gonçalves Soares <dan13lgonc@gmail.com>
2021-07-17 13:52:00 -0300

    Criacao-App-Barra

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,111 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
+   <v-card id="lateral">
+    <v-toolbar
       dark
+      tabs
+      flat
+      color="indigo"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Saúde Para Todos</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
-    </v-app-bar>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs
+          v-model="tabs"
+          align-with-title
+        >
+          <v-tab href="#one">
+            Início
+          </v-tab>
+          <v-tab href="#two">
+            Profissionais
+          </v-tab>
+          <v-tabs-slider color="pink"></v-tabs-slider>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+    <v-card-text>
+      <v-tabs-items v-model="tabs">
+        <v-tab-item
+          v-for="content in ['one', 'two', 'three']"
+          :key="content"
+          :value="content"
+        >
+          <v-card
+            height="200px"
+            flat
+          >
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card-text>
+    <v-fab-transition>
+      <v-btn
+        :key="activeFab.icon"
+        :color="activeFab.color"
+        fab
+        large
+        dark
+        bottom
+        left
+        class="v-btn--example"
+      >
+        <v-icon>{{ activeFab.icon }}</v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </v-card>
 
     <v-main>
-      <HelloWorld/>
+      <Inicio/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import Inicio from './components/Inicio.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Inicio,
   },
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+      fab: false,
+      hidden: false,
+      tabs: null,
+    }),
+
+    computed: {
+      activeFab () {
+        switch (this.tabs) {
+          case 'one': return { color: 'success', icon: 'mdi-share-variant' }
+          case 'two': return { color: 'red', icon: 'mdi-pencil' }
+          case 'three': return { color: 'green', icon: 'mdi-chevron-up' }
+          default: return {}
+        }
+      },
+    },
 };
 </script>
+
+<style>
+  /* This is for documentation purposes and will not be needed in your application */
+  #lateral .v-btn--example {
+    bottom: 0;
+    position: absolute;
+    margin: 0 0 16px 16px;
+  }
+  .mdi-menu::before{
+    display: none;
+  }
+</style>
