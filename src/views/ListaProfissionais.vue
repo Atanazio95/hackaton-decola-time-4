a0f37d820676ae3eb1baeca3f1619358f04f1e10
Vinicius Santos Matos <ovinidev@gmail.com>
2021-07-18 01:12:35 -0300

    Alteração

--- src/views/ListaProfissionais.vue
+++ src/views/ListaProfissionais.vue
@@ -1,94 +1,77 @@
 <template>
-  <v-card
-    max-width="450"
-    class="mx-auto"
-  >
-    <v-toolbar
-      color="cyan"
-      dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Inbox</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-list three-line>
-      <template v-for="(item, index) in items">
-        <v-subheader
-          v-if="item.header"
-          :key="item.header"
-          v-text="item.header"
-        ></v-subheader>
-
-        <v-divider
-          v-else-if="item.divider"
-          :key="index"
-          :inset="item.inset"
-        ></v-divider>
-
-        <v-list-item
-          v-else
-          :key="item.title"
-        >
-          <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title v-html="item.title"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list>
-  </v-card>
+  <v-simple-table>
+    <template v-slot:default>
+      
+      <tbody>
+          <tr  v-for="profissional of listaProfissionais" :key="profissional.nome">
+            <v-container class="profissional-lista">
+                 <td>
+                    <v-avatar center>
+                    <img :src="profissional.imagem" class="my-10">
+                    </v-avatar>
+                  <div class="perfil-info">
+                    {{ profissional.nome }}
+                  </div>
+                  <div class="perfil-info">
+                    {{ profissional.signo }}
+                  </div>
+                  <div class="perfil-info">
+                    {{ profissional.servicos }}
+                  </div>
+                  <div class="perfil-info">
+                    <v-button class="botao"><v-icon left>mdi-message</v-icon></v-button>
+                    <v-button><v-icon>mdi-phone-in-talk</v-icon></v-button>
+                    <v-button><v-icon>mdi-message-video</v-icon></v-button>
+                    <v-button><v-icon>mdi-star</v-icon></v-button>
+                  </div>
+                  </td>
+            </v-container>
+ 
+        </tr>
+        
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
-
+ 
 <script>
   export default {
-    data: () => ({
-      items: [
-        { header: 'Today' },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'Birthday gift',
-          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        },
-      ],
-    }),
+ 
+    name: 'ListaProfissionais',
+    
+    data () {
+      return{
+        listaProfissionais: []
+      }
+    },
+    created() {
+      fetch('https://it3-gdf-default-rtdb.firebaseio.com/saude/profissionais.json')
+      .then(resposta => resposta.json())
+      .then(json => {
+        this.listaProfissionais = json
+        console.log(this.listaProfissionais);
+      });
+    }
   }
 </script>
-
+ 
 <style>
-
-</style>
+   .profissional-lista{
+    height: 250px;
+    border: solid grey 2px;
+    padding: 10vw;
+  }
+  .botao{
+    padding: 5hvw;
+    text-align: center;
+  }
+  .perfil-info{
+    text-align: center;
+    display: flex;
+    justify-content: space-around;
+    padding-top: 8px;
+  }
+  img{
+    text-align: center;
+  }
+ </style>
